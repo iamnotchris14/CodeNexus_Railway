6484d92bb56f9e62612bfce0bd4fe0af4a09ad30
Hashika <hkeshreen30@gmail.com>
2025-07-15 20:17:03 +0800

    Hashika user interface

--- main.cpp
+++ main.cpp
@@ -10,15 +10,34 @@ int main() {
 
     // algorithm and user input
     string source, destination;
+    int choice;
 
     cout << "==========================="<<endl;
     cout << "Klang Valley Transit Route Planner" << endl;
     cout << "===========================" << endl;
     cout<< "\tKajang Line" << endl;
-    cout << "Enter source station: ";
-    getline(cin, source);
-    cout << "Enter destination station: ";
-    getline(cin, destination);
+    do {
+        cout << "Enter source station: ";
+        getline(cin, source);
+        cout << "Enter destination station: ";
+        getline(cin, destination);
+
+        cout << "\nCalculating shortest route..." << endl;
+        cout << "Shortest route found!" << endl;
+
+        //Options
+        cout << "1 - Display shortest route only\n"
+                "2 - Display all routes\n"
+                "3 - Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: "\nRoute: " << endl;
+                    "Total time taken: " << endl;
+
+        }
+    }while (choice != 3);
 
     return 0;
 }
